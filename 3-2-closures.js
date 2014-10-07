0b9d3028be518250a59270698381608571b5c4aa
Jennie Kim <jk@udacity.com>
2014-10-07 14:47:03 -0700

    added warningMaker function with zones and nums

--- 3-2-closures.js
+++ 3-2-closures.js
@@ -29,3 +29,34 @@ function warningMaker( obstacle ){
   };
 }
 
+
+// warningMaker function with locations and number of sightings
+
+  var nums = [];
+  return function ( number, location ) {
+    count++;
+    zones.push(location);
+    nums.push(number);
+    var list = "";
+    for(var i = 0; i<zones.length; i++){        
+      list = list + "\n" + zones[i] + " (" + nums[i] + ")";  
+    }
+    alert("Beware! There have been " +
+          obstacle +
+          " sightings in the Cove today!\n" +
+          number +
+          " " +
+          obstacle +
+          "(s) spotted at the " +
+          location +
+          "!\n" +
+          "This is Alert #" +
+          count +
+          " today for " +
+          obstacle +
+          " danger.\n" +
+          "Current danger zones are: " +
+          list
+         );
+  };
+}

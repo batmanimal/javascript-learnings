409ca4cead99d5aafb9fb2c4d4556604268029b4
Jennie Kim <jk@udacity.com>
2014-10-07 20:04:18 -0700

    Update 3-3-hoisting.js

--- 3-3-hoisting.js
+++ 3-3-hoisting.js
@@ -20,6 +20,10 @@ function theBridgeOfHoistingDoom( ){
   return sword;
 }
 
+// what gets returned?
+/* Too tough? Okay, before the return statement is ever reached, the ring variable is called as a function. But since that variable does not have any contents by the time it is asked to be a function, the program will return a ‘Type Error’ saying that ring is not yet a function. theBridgeOfHoistingDoom will therefore be unable to complete, or reach the return of sword as expected.
+*/
+
 // original version
 function theBridgeOfHoistingDoom( ){
   function fellowship(){

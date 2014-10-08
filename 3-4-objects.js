cb31dc7de4d1b77b0b88ab6ae912c5c499de2751
Jennie Kim <jk@udacity.com>
2014-10-07 21:57:12 -0700

    Update 3-4-objects.js

--- 3-4-objects.js
+++ 3-4-objects.js
@@ -22,3 +22,31 @@ var findVehicle = function (name, list) {
 
 findVehicle("Submarine", vehicles);
 
+// changing objects
+/* They need to add some important information and power upgrades to their existing vehicles. Using only dot notation and new properties weapon (which gets a string) and submersible (which gets a boolean), as well as existing properties, make the following changes and additions, in order, to the Ocean’s transportation:
+
+4 seats will be added to the sides of the Motorboat, changing the amount of people it can carry.
+The Jet Ski is not submersible.
+The Submarine will acquire “Torpedoes”.
+The Motorboat is not submersible.
+The Jet Ski will be fitted with new pirate-zapping “Lasers”.
+Bunk beds will be placed in the Submarine, doubling its potential staff.
+A “Rear-Mounted Slingshot” will be added to the Motorboat.
+The Submarine is submersible.
+The existing objects are provided for your reference. When upgrading weapons, use the exact string found in the list.
+*/
+
+var vehicle1 = {type: "Motorboat", capacity: 6, storedAt: "Ammunition Depot"};
+var vehicle2 = {type: "Jet Ski", capacity: 1, storedAt: "Reef Dock"};
+var vehicle3 = {type: "Submarine", capacity: 8, storedAt: "Underwater Outpost"};
+
+vehicle1.weapon = "Rear-Mounted Slingshot";
+vehicle1.submersible = false;
+vehicle1.capacity = vehicle1.capacity + 4;
+
+vehicle2.weapon = "Lasers";
+vehicle2.submersible = false;
+
+vehicle3.weapon = "Torpedoes";
+vehicle3.submersible = true;
+vehicle3.capacity = vehicle3.capacity*2;

018d2c76a057656443d20b355b5897ab02a03556
Jennie Kim <jk@udacity.com>
2014-10-08 20:04:38 -0700

    Update 3-4-objects.js

--- 3-4-objects.js
+++ 3-4-objects.js
@@ -216,3 +216,26 @@ function listGuns (guns) {
 }
 
 listGuns(rockSpearguns);
+
+// Enumeration 3
+
+var rockSpearguns = {
+  Sharpshooter: {barbs: 2, weight: 10, heft: "overhand"},
+  Pokepistol: {barbs: 4, weight: 8, heft: "shoulder"},
+  Javelinjet: {barbs: 4, weight: 12, heft: "waist"},
+  Firefork: {barbs: 6, weight: 8, heft: "overhand"},
+  "The Impaler": {barbs: 1, weight: 30, heft: "chest"}
+};
+
+rockSpearguns["listGuns"] = function (){
+  for(var property in this){
+    if(this[property]["heft"] != undefined) {
+      console.log("Behold! " + property +
+                ", with " + this[property]["heft"] +
+                " heft!");
+    }
+  }
+};
+
+rockSpearguns["listGuns"]();
+

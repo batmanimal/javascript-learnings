d1e4ece48e7427e9450fe65a8c48c508f169840e
Jennie Kim <jk@udacity.com>
2014-10-15 16:09:18 -0700

    Create eloquent-5-range-sum-fcn.js

---
+++ eloquent-5-range-sum-fcn.js
@@ -0,0 +1,24 @@
+var sum = function (array) {
+  sumOf = 0;
+  for (var i = 0; i < array.length; i++) {
+    sumOf += array[i];
+  }
+  return sumOf;
+};
+
+function range (start, end, step) {
+  var rangeArray = [start];
+  var i = start;
+  while (i < end - step) {
+    i = i + step;
+    rangeArray.push(i);
+  }
+  return rangeArray;
+}
+
+console.log(sum(range(1,100,10)));
+
+// console.log(range(1, 10, 5));
+
+//console.log(sum([1,2,3,4,5]));
+

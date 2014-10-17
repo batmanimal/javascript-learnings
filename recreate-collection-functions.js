2a9979c7194217377f233a27b4b34c9463066ee6
Jennie Kim <jk@udacity.com>
2014-10-17 16:29:11 -0700

    Create recreate-collection-functions.js

---
+++ recreate-collection-functions.js
@@ -0,0 +1,20 @@
+// Filter
+// Params: array, predicate (function that tests something on each item) 
+
+// takes in an array and a predicate (test) that items in the array must pass before being pushed into the resulting array
+
+function filter (array, test) { 
+  var passed = [];
+  for (var i = 0; i < array.length; i++) {  // for loop - could use forEach function here 
+    if (test(array[i]))
+      passed.push(array[i]);
+  }
+  return passed;
+}
+
+// call the function. created simple test condition to return elements < 1. 
+
+console.log( filter([0,0,0,1], function (x) {return x < 1; }) );
+// [0, 0, 0]
+
+

f17385dea93fb1567ffcc1d6913bfb260b78ff72
Jennie Kim <jk@udacity.com>
2014-10-16 21:24:52 -0700

    Update codecademy-fixLetterCase-recursion.js

--- codecademy-fixLetterCase-recursion.js
+++ codecademy-fixLetterCase-recursion.js
@@ -22,3 +22,33 @@ function fixLetterCase(array, i) {
 fixLetterCase(capitals, 0);
 
 console.log(capitals);
+
+
+var capitals = [ ["berlin", "parIs", "MaDRiD"], 
+                 ["monTEvideo", "BrASiLia"],
+                 ["dElhi", "toKYo", "BeiJing"],
+                 ["CanBerra"],
+                 ["kiGaLi", "pretoRIA"] ];
+
+// Capitalize function
+function capitalize(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+}
+
+// Our recursive function
+function fixLetterCase(array, x, y) {
+  if (y === array.length) {
+    return;
+  }
+  else if (x === array[y].length) {
+    return fixLetterCase(array, 0, y + 1);
+  }
+  else {
+    array[y][x] = capitalize(array[y][x]);
+    return fixLetterCase(array, x + 1, y);
+  }	
+}
+
+fixLetterCase(capitals, 0, 0);
+
+console.log(capitals);

479189e3186718df6013d7c7a235ddc22797e83d
Jennie Kim <jk@udacity.com>
2014-10-16 21:40:35 -0700

    Update and rename codecademy-guessNumber.js to codecademy-guessNumber-recursion-while.js

--- codecademy-guessNumber-recursion-while.js
+++ codecademy-guessNumber-recursion-while.js
@@ -19,3 +19,26 @@ function guessNumber(number, clue) {
 }
 
 guessNumber(13, "Guess a number between 1 and 25.");
+
+// with while loop
+
+function guessNumber(number, clue) {
+  // Prompt the user for a number using the string value of clue
+  guess = prompt(clue);
+  // Convert their guess to a number using +
+  guess = +guess;
+  
+  while (guess !== number) {
+    if (guess < number) {
+      guess = prompt("Too low. Guess again.");
+    }
+    else if (guess > number) {
+      guess = prompt("Too high. Guess again.");
+    }
+    guess = +guess;
+  }
+  
+  console.log("You got it! The number was " + number);
+}
+
+guessNumber(13, "pick a number from 1-20");

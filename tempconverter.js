21e7f46c372a43c1da35fd8955e306f987627e19
thejenniekim <jk@udacity.com>
2015-03-05 17:35:59 -0800

    made it work

--- tempconverter.js
+++ tempconverter.js
@@ -1,45 +1,53 @@
+// build function to convert from temp to temp in K
+
 var convert = {
+	// store functions as objects within the convert object 
 	fToK: function(temp) {
-
+		return ((5*(temp-32) /9) + 273.15);
 	},
-	kToF: function(temp) {
-
+	cToK: function(temp) {
+		return temp+273.15;
 	},
 	kToK: function(temp) {
 		return temp;
+	},
+	kToF: function(temp) {
+		return (9/5)*(temp-273.15) + 32;
+	},
+	kToC: function(temp) {
+		return temp-273.15;
 	}
-}
+};
 
 
-function tempConversion (config) {
+function tempConverter (config) {
 
 	// first, convert to your bridge temperature ('K')
-	if (config.from === 'F') {
-		myTemp = convert.fToK(config.value);
-	}
-
-	tempInK = convert[config.from.toLowerCase() + 'ToK'](config.value);
+	var tempInK = convert[config.from.toLowerCase() + 'ToK'](config.value);
 
 	// second, convert the 'K' temperature to your 'to' temperature
-
-}
-
-
+	return convert['kTo' + config.to.toUpperCase()](tempInK);
+};
 
 
 
-
-
-// convert 0 C to F
-var a = tempConversion({
-	value: 0,
+// convert 10 C to F
+var a = tempConverter({
+	value: 10,
 	from: 'C',
 	to: 'F'
 });
 
 // convert 0 C to K
-var b = tempConversion({
+var b = tempConverter({
 	value: 58.2,
 	from: 'F',
-	to: 'K'
+	to: 'C'
 })
+
+// test 
+console.log(a);
+// 50
+console.log(b);
+
+

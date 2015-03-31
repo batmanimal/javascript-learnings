496c99641e5d11b1114a775382eb881d4d2ea2c4
Jennie Kim <thejenniekim@gmail.com>
2015-03-31 09:49:15 -0700

    new file to implement lodash

---
+++ lodash_practice.js
@@ -0,0 +1,28 @@
+// make an immediately invoked function expression 'IFFE'
+(function() {
+	// declare undefined 
+    var undefined;
+	
+	// declare version number as a GLOBAL, decreases chance of accidental use
+    var VERSION = '3.6.0';
+    
+    // TODO add the other GLOBAL variables
+    // TODO add the Object#toString and arrayBufferTag variables
+
+    // match empty string literals in compiled template source
+    // TODO understand when I would use this 
+    var reEmptyStringLeading = /\b__p \+= '';/g;
+    	// TODO the rest of the empty string matches 
+
+    // TODO everything above the first line in lodash!
+    /*----------------------------------------------------*/
+
+
+// call lodash
+}.call(this));
+
+
+console.log(NaN===NaN);
+// false
+console.log(3/0===4/0);
+// true

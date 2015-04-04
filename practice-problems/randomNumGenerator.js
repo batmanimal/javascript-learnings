cb738e82bfc54d08c126da67e820f87c48db6006
Jennie Kim <thejenniekim@gmail.com>
2015-04-04 07:39:28 -0700

    fixed a major error with how range is calculated
    previously returned a number starting at 0, and up to and including the
    abs value of the difference between numbers. Neglected to start the
    “range” from the bottom num (so the “range” from 10-15 became 0-5!).
    Also got rid of the word “range” bc it was confusing as to what the
    bottom and top values were.

--- practice-problems/randomNumGenerator.js
+++ practice-problems/randomNumGenerator.js
@@ -1,29 +1,41 @@
-// collect 2 numbers from user
-// print a random whole number that falls between these two numbers
 // TODO: deal with non-number inputs
 
-// collect num1
+// collect num1 from user
 var num1 = parseInt(prompt("Give me a number!"));
 
-// collect num2
+// collect num2 from user
 var num2 = parseInt(prompt("Give me a different number!"));
+
 // do not accept num2 if it is equal to num1
-while(num2===num1){
+while (num2 === num1){
   num2 = parseInt(prompt("Give me a DIFFERENT number!")); 
 }
+
 // build a func to subtract the smaller num from the larger num
-var range = function(a,b){
-  if(b>a) {
+var sortNums = function (a,b){
+  if (b > a){
     return b-a;
   }
   else { return a-b; }
 };
-// invoke the range function on num1 and num2 
-var rangeOfNums = range(num1,num2);
+// invoke the sortNums function on num1 and num2 
+var differenceOfNums = sortNums(num1, num2);
+
+// find the smaller num
+var returnSmallerNum = function (a,b){
+	if (b < a){
+		return b;
+	} 
+	else { return a; }
+};
+
+// create variable to store the smaller of the two nums
+var smallerNum = returnSmallerNum(num1, num2);
 
 // use Math methods to generate random num 
 // store in variable to return to user
-var randomNum = Math.floor(Math.random()*rangeOfNums) +1;
+// need to start from the smaller number, rather than start from 0
+var randomNum = Math.floor(Math.random() * (differenceOfNums + 1)) + smallerNum;
 
 // alert the randomNum to user
 alert("Your number is " + randomNum);
